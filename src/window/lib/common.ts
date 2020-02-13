0993f003e5680fc0fa94ea4d7f3b80c3da367c4f
kohei.hujita <point.out92@gmail.com>
2020-02-14 03:25:54 +0900

    change icon

--- src/window/lib/common.ts
+++ src/window/lib/common.ts
@@ -1,8 +1,7 @@
-import { ISize } from "../definition/ISize";
 import * as path from "path";
 
 export default class Common {
   public static backgroundColor: string = "#23282f";
   public static mainURL: string = path.join(__dirname, "../../index.html");
-  public static iconPath: string = path.join(__dirname, "../../../../resources/image/pen-icon.png");
+  public static iconPath: string = path.join(__dirname, "../../../../resources/image/dr.png");
 }
