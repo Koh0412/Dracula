80429217fe2f96a50d51e0b58743a0f0f1dbc6c3
kohei.hujita <point.out92@gmail.com>
2020-02-15 04:40:44 +0900

    fix innerHTML

--- src/renderer/lib/render.ts
+++ src/renderer/lib/render.ts
@@ -26,6 +26,7 @@ class Render {
     });
 
     ipcRenderer.on(ICPKeys.save.path, (_, filePath: string) => {
+      this.footer.innerHTML = "the file has been saved";
       setTimeout(() => {
         this.footer.innerHTML = filePath;
       }, 1500);
