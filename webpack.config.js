0212bd21cfd5772f5e4446e3f814feffa069de4b
kohei.hujita <point.out92@gmail.com>
2020-04-09 23:27:56 +0900

    remane folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   // devtool: 'inline-source-map',
-  entry: './src/renderer/index.ts',
+  entry: './src/browser/index.ts',
   target: 'electron-renderer',
   watchOptions: {
     ignored: /node_modules/
   },
   output: {
     filename: 'index.js',
-    path: path.join(__dirname, './build/src/renderer'),
+    path: path.join(__dirname, './build/src/browser'),
   },
   resolve: {
     extensions: ['.ts', '.js']
